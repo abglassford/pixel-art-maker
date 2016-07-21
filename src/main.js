c2424065c775f6da1253504ed5415e47e4f13a77
Alexander Glassford <a.b.glassford@gmail.com>
2016-07-21 11:25:56 -0600

    add dynamic funtionality to pallete generator

--- src/main.js
+++ src/main.js
@@ -11,18 +11,19 @@ $(function() {
   }
   newCanvas();
 
-  function newPallate() {
+  function newPallete() {
     var paint;
     var pallete = $('#pallete');
-    var colors = ['red', 'blue', 'green', 'yellow', 'orange', 'violet']
+    var colors = ['pink', 'purple', 'LimeGreen', 'black', 'white', 'yellow', 'red', 'blue']
 
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < colors.length; i++) {
       pallete.append($('<div class="paint ' + colors[i] + '"></div>'));
+      $('.' + colors[i]).css('background-color', colors[i])
     }
   }
-  newPallate();
+  newPallete();
   var color
-  
+
   $('.pixel').on('click', function(e){
     $(this).css('background-color', color);
   })
